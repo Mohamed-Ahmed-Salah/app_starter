b2f6d8cf67e513a4a50962b0906e238be37abf1e
msalah8 <m.salah@marcomarabia.net>
2025-10-21 14:35:57 +0300

    update failurePopups

--- lib/config/network/mixins/failure_popups.dart
+++ lib/config/network/mixins/failure_popups.dart
@@ -1,28 +1,27 @@
-
 import 'package:app_starter/config/errors/error_const.dart';
 import 'package:app_starter/config/errors/failures.dart';
 
 mixin FailurePopups{
-  void showToast(Failure failure , bool isEn){
+  String getFailureMessage(Failure failure, bool isEn) {
     if (failure is GeneralFailure) {
-      // UtilFunctions.showSnackBar(
-      //   message:
-      //   "${failure.message} ${failure.statusCode} \n${failure.errors.join('\n')}",
-      // );
+      return "${failure.message} ${failure.statusCode} \n${failure.errors.join('\n')}";
     } else if (failure is NoInternetFailure) {
       String message = isEn
           ? ErrorConst.noInternetMessageEn
           : ErrorConst.noInternetMessageAr;
-      // UtilFunctions.showSnackBar(message: "$message ${failure.statusCode}");
+      return "$message ${failure.statusCode}";
     } else if (failure is TimeOutFailure) {
       String message = isEn
           ? ErrorConst.timeoutMessageEn
           : ErrorConst.timeoutMessageEn;
-      // UtilFunctions.showSnackBar(message: "$message ${failure.statusCode}");
+      return "$message ${failure.statusCode}";
     } else {
-      // UtilFunctions.showSnackBar(
-      //   message: "${failure.message} ${failure.statusCode}",
-      // );
+      return "${failure.message} ${failure.statusCode}";
     }
+  }
+
+  void showToast(Failure failure, bool isEn) {
+    final message = getFailureMessage(failure, isEn);
+    // UtilFunctions.showSnackBar(message: message);
   }
 }
